b4042d9d86f278d3ced2534199d3e52e6bb902aa
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-08-23 14:53:23 -0500

    Add project files.

---
+++ BetterGuestBook/GuestInformation.cs
@@ -0,0 +1,56 @@
+﻿using GuestBookLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterGuestBook
+{
+    internal class GuestInformation
+    {
+        public static void GetGuestInformation(List<GuestModel> guests)
+        {
+            
+            string MoreGuestComing = "";
+
+            do
+            {
+                GuestModel guest = new GuestModel();
+                
+                guest.FirstName = GetInformationFromConsole("What is your first name? ");
+
+                guest.LastName = GetInformationFromConsole("What is your last name? ");
+
+                guest.MessageToHost = GetInformationFromConsole("What message would you like to tell  your host? ");
+
+                MoreGuestComing = GetInformationFromConsole("Are more guest coming (yes/no): ");
+
+                guests.Add(guest);
+
+                Console.Clear();
+
+            } while (MoreGuestComing.ToLower() == "yes");
+        }
+
+        public static string GetInformationFromConsole(string message)
+        {
+            string output = "";
+
+            Console.WriteLine(message);
+            output = Console.ReadLine();
+
+            return output;
+        }
+
+        public static void PrintGuestInformation(List<GuestModel> guests)
+        {
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
+        }
+
+        
+    }
+}
---
+++ BetterGuestBook/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using BetterGuestBook;
+
+using GuestBookLibrary;
+using GuestBookLibrary.Models;
+
+
+// Capture the information about each guest (assumption at least one guest and unknown maximum)
+// Info to capture: First Name, Last Name, message to host
+// Once done, loop through each guest and print their info
+
+
+List<GuestModel> guests = new List<GuestModel>();
+
+GuestInformation.GetGuestInformation(guests);
+
+GuestInformation.PrintGuestInformation(guests);
+
+Console.ReadLine();
